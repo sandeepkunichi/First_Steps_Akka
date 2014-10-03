2b029fb87a0d95a4e1e86cca6186661424a96a36
Sandeep Kunichi <sandeepkunichi@gmail.com>
2014-10-03 12:35:33 +0530

    Actor system using push down method
    A three actor system that splits the work of
    1. Web crawling - Fetch data from the web
    2. Data entry - Put data onto a file
    The work is split between the three actors to reduce overhead and
    improve throughput.

---
+++ three_ActorSystem.scala
@@ -0,0 +1,107 @@
+package com.tAS.test
+
+
+import akka.actor._
+import scala.xml.Node
+import java.io._
+
+case object StartMessage
+case object StopMessage
+case object next
+
+class Actor1(actor2: ActorRef) extends Actor {				//Link generator
+  var num=2000000
+  def receive = {
+    
+    case StartMessage =>
+      val link = "http://www.imdb.com/title/tt"+num
+      actor2 ! link
+      self ! next
+      
+    case next =>
+      num+=1
+      if(num<=2000010)
+      {
+      val link = "http://www.imdb.com/title/tt"+num
+      actor2 ! link
+      self ! next
+      }
+      else
+      {
+        actor2 ! StopMessage
+        context.stop(self)
+      }
+      
+  }
+}
+ 
+class Actor2(actor3: ActorRef) extends Actor {			//Content fetcher
+  
+  def attributeEquals(name: String)(node: Node) = node.attribute(name).isDefined
+  
+  def getContent(link:String):String = {
+		val parserFactory = new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+		val parser = parserFactory.newSAXParser()
+		val source = new org.xml.sax.InputSource(link)
+		val NBFA = new scala.xml.parsing.NoBindingFactoryAdapter
+		val res = NBFA.loadXML(source, parser)
+		(res \\ "h1").text+" - "+(res \\ "p").filter(attributeEquals("itemprop")).text
+	}
+  
+  def receive = {
+    
+    case link =>
+      try{
+      val content = getContent(link.toString)
+      actor3 ! content
+      }
+      catch {
+         case ex: FileNotFoundException =>{
+            println("Missing file exception")
+         }
+      }
+      
+    case StopMessage =>
+      actor3 ! StopMessage
+      context.stop(self)
+      
+  }
+  
+}
+class Actor3 extends Actor { 										//Content writer
+  
+	def using[A <: {def close(): Unit}, B](param: A)(f: A => B): B = try { f(param) } finally { param.close() }
+	
+	def appendToFile(fileName:String, textData:String) =
+	  using (new FileWriter(fileName, true)){
+		fileWriter => using (new PrintWriter(fileWriter)) {
+		  printWriter => printWriter.println(textData)
+		}
+	}
+	
+	var num = 1
+
+	def receive = {
+	  
+	  case content =>
+	    //outWriteContent(content.toString)
+	    appendToFile("MovieDB(small).txt",num+" "+content.toString)
+	    println("Done printing onto the file, job number: "+num)
+	    num+=1
+	    
+	  case StopMessage =>
+	    context.stop(self)
+	
+	}
+}
+ 
+object doJob extends App {
+  
+  val system = ActorSystem("3ActorSystem")
+  val actor3 = system.actorOf(Props[Actor3], name = "actor3")
+  val actor2 = system.actorOf(Props(new Actor2(actor3)), name = "actor2")
+  val actor1 = system.actorOf(Props(new Actor1(actor2)), name = "actor1")
+  //start the system
+  actor1 ! StartMessage
+  
+}
